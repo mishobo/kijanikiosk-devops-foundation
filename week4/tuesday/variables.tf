9e03a0f4c85cee9d76a58f33e7734134eb4abdcc
mishobo <husseinabdallah287@gmail.com>
2026-07-04 15:26:27 +0300

    doc: tuesdays lab

--- week4/tuesday/variables.tf
+++ week4/tuesday/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy infrastructure into"
   type        = string
-  default     = "eu-west-1"    # Frankfurt - closest to Nairobi with EC2 free tier
+  default     = "ap-south-1"    # Mumbai - closest to Nairobi with EC2 free tier
 }
 
 variable "instance_type" {
